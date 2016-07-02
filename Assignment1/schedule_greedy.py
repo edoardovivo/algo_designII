6a1ee6449680130357c936c1d0d031e0f7d1e5bd
Edoardo Vivo <vivoedoardo@gmail.com>
2016-07-02 15:18:38 +0200

    Addedo solutions for problems 1 and 2

--- Assignment1/schedule_greedy.py
+++ Assignment1/schedule_greedy.py
@@ -30,6 +30,11 @@ For this problem, use the same data set as in the previous problem.
 
 Your task now is to run the greedy algorithm that schedules jobs (optimally) in decreasing order of the ratio (weight/length). In this algorithm, it does not matter how you break ties. You should report the sum of weighted completion times of the resulting schedule --- a positive integer --- in the box below.
 
+
+SOLUTIONS:
+Diff:  69119377652
+Ratio:  67311454237
+
 '''
 
 
