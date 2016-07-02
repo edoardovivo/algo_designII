8f690edbcdcb5bfc94e436cc01f81d6fbc73b109
Edoardo Vivo <vivoedoardo@gmail.com>
2016-07-02 15:42:27 +0200

    Added problem statement and removed reduntant python file

--- Assignment2/clustering_big.pyx
+++ Assignment2/clustering_big.pyx
@@ -1,6 +1,29 @@
-import csv
-import numpy as np
-import UnionFind
+'''
+In this question your task is again to run the clustering algorithm from lecture, but on a MUCH bigger graph. So big, in fact, that the distances (i.e., edge costs) are only defined implicitly, rather than being provided as an explicit list.
+
+The data set is below.
+
+clustering_big.txt
+The format is:
+
+[# of nodes] [# of bits for each node's label]
+
+[first bit of node 1] ... [last bit of node 1]
+
+[first bit of node 2] ... [last bit of node 2]
+
+...
+
+For example, the third line of the file "0 1 1 0 0 1 1 0 0 1 0 1 1 1 1 1 1 0 1 0 1 1 0 1" denotes the 24 bits associated with node #2.
+
+The distance between two nodes u and v in this problem is defined as the Hamming distance--- the number of differing bits --- between the two nodes' labels. For example, the Hamming distance between the 24-bit label of node #2 above and the label "0 1 0 0 0 1 0 0 0 1 0 1 1 1 1 1 1 0 1 0 0 1 0 1" is 3 (since they differ in the 3rd, 7th, and 21st bits).
+
+The question is: what is the largest value of k such that there is a k-clustering with spacing at least 3? That is, how many clusters are needed to ensure that no pair of nodes with all but 2 bits in common get split into different clusters?
+
+NOTE: The graph implicitly defined by the data file is so big that you probably can't write it out explicitly, let alone sort the edges by cost. So you will have to be a little creative to complete this part of the question. For example, is there some way you can identify the smallest distances without explicitly looking at every pair of nodes?
+
+
+'''
 
 
 def invert(bit):
@@ -12,22 +35,24 @@ def invert(bit):
 def similar(v):
     out = []
     for i in range(len(v)):
-        out.append(v[:i]+invert(v[i]) + v[i+1:])
-        for j in range(i+1, len(v)):
-            out.append(v[:i]+invert(v[i])+v[i+1:j]+invert(v[j])+v[j+1:])
+        out.append(v[:i] + invert(v[i]) + v[i + 1:])
+        for j in range(i + 1, len(v)):
+            out.append(v[:i] + invert(v[i]) + v[i + 1:j] +
+                       invert(v[j]) + v[j + 1:])
     return out
 
 
 def main2():
-    vertices = ["".join(x.split(' ')) for x in open('clustering_big.txt', 'r').read().split('\n')[0:-1]]
+    vertices = ["".join(x.split(' ')) for x in open(
+        'clustering_big.txt', 'r').read().split('\n')[0:-1]]
     heads = {}
     for v in vertices:
         heads[v] = v
-    clusters = len(heads) 
+    clusters = len(heads)
 
     cnt = 0
     for v in vertices:
-        cnt +=1 
+        cnt += 1
         print cnt
         v_head = heads[v]
         while heads[v_head] != v_head:
@@ -35,9 +60,7 @@ def main2():
 
         for friend in similar(v):
             if heads.get(friend):
-                #print "inside"
-                #heads[v_head].append(friend)
-                #heads.pop(friend, None)
+
                 head = heads[friend]
                 while heads[head] != head:
                     head = heads[head]
