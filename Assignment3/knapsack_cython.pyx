f2257ebb79650188e6a4d59f70fd45276d41de7b
Edoardo Vivo <vivoedoardo@gmail.com>
2016-07-02 17:13:02 +0200

    Optimized solution for problem 2

--- Assignment3/knapsack_cython.pyx
+++ Assignment3/knapsack_cython.pyx
@@ -1,16 +1,18 @@
-cimport numpy as np
 import numpy as np
+cimport numpy as np
 
-cpdef double knapsack(np.ndarray values, np.ndarray weights):
-    cdef int W = 10000
-    cdef np.ndarray[double] previous = np.zeros((W+1,))
-    cdef np.ndarray[double] current = np.zeros((W+1,))
+
+cpdef np.float knapsack(int W, np.ndarray[double, ndim=1] values, np.ndarray[double, ndim=1] weights):
+    cdef np.ndarray[double] previous = np.zeros((W + 1,))
+    cdef np.ndarray[double] current = np.zeros((W + 1,))
     cdef int i, x, n = len(values)
     cdef int ind_weight = 0
-    for i in xrange(1,n):
-        weight = weights[i]
+    cdef int weight = 0
+    cdef double value = 0
+    for i in xrange(1, n):
+        weight = int(weights[i])
         value = values[i]
-        for x in xrange(0,W+1):
+        for x in xrange(0, W + 1):
             ind_weight = x - weight
             if (ind_weight >= 0):
                 current[x] = max(previous[x], previous[ind_weight] + value)
