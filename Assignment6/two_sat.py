8bf3df1eab4dae95d7a3fe5e9c1f3fbdc3f152a8
Edoardo Vivo <vivoedoardo@gmail.com>
2016-07-20 17:29:27 +0200

    Implementation of DFS complete

--- Assignment6/two_sat.py
+++ Assignment6/two_sat.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import two_sat_cython as tsc
 
 def read_2sat(fname):
     first_line = open(fname).readline().strip('\n').split(' ')
@@ -10,84 +10,78 @@ def read_2sat(fname):
     return (n_vars, sat_conditions.astype(int))
 
 
-def make_graph(n_vars, sat_conditions):
-    
-    keys = [x for x in range(-n_vars, n_vars+1) if x != 0]
-    graph = {k: [] for k in keys}
+def read_make_graph(fname):
+    first_line = open(fname).readline().strip('\n').split(' ')
+    n_vars = int(first_line[0])
+    sat_conditions = np.loadtxt(fname, skiprows=1)
+    graph = {}
     for i in range(n_vars):
         x1 = sat_conditions[i, 0]
         x2 = sat_conditions[i, 1]
-        try:
-            graph[-x1].append(x2)
-        except:
-            graph[-x1] = [x2]
-        try:
-            graph[-x2].append(x1)
-        except:
-            graph[-x2] = [x1]
-    
-    graph = {k: set(v) for k,v in graph.iteritems()}
+        if (int(-x1) in graph):
+            graph[int(-x1)].append(int(x2))
+        else:
+            graph[int(-x1)] = [int(x2)]
+        if (int(-x2) in graph):
+            graph[int(-x2)].append(int(x1))
+        else:
+            graph[int(-x2)] = [int(x1)]
+    graph = {k: set(v) for k, v in graph.iteritems()}
     return graph
 
 
+def check(visited_order):
+    visited_opposite = set([-x for x in visited_order])
+    s = set(visited_order).intersection(visited_opposite)
+    if not s:
+        return True
+    else:
+        print s
+        return False
+
 
 def DFS_connected_components(graph):
-    visited = {k: False for k in graph}
-    nodes_to_visit = graph.keys()
-    connected_components = {}
-    while nodes_to_visit:
-        # I want to start the search from the most connected node
-        start = max(nodes_to_visit, key=lambda x: len(graph[x]))
-        if (len(graph[start]) == 0):
-            visited[start] = True
-            connected_components[start] = [start]
-            nodes_to_visit.remove(start)
+    n = len(graph)
+    visited = []
+    while n > 0:
+        start = graph.iterkeys().next() #max(graph.keys(), key=lambda x: len(graph[x]))
+        visited_order = DFS(graph, start, 'direct')
+        # We need to check if -x and x appear in the same connected component.
+        if check(visited_order):
+            visited.extend(visited_order)
+            graph = {k: v for k, v in graph.iteritems() if k not in visited}
+            n = len(graph)
+            print n, len(visited_order)
         else:
-            #start = nodes_to_visit[0]
-            visited, visited_order = DFS(graph, start, 'direct', visited=visited)
-            connected_components[start] = visited_order
-            # We need to check if -x and x appear in the same connected component.
-            # If so, we need to check if they are in the same STRONGLY connected component.
-        nodes_to_visit = [node for node, v in visited.iteritems() if not v]
-        print len(nodes_to_visit)
-    return connected_components
+            print "Not Satisfiable!"
+            print visited_order
+            return False
+    return True
 
 
-def DFS(graph, start, mode, visited=None):
+def DFS(graph, start, mode):
     # if m=0 we are looking at the reverse graph
     S = []
     visited_order = []
+    visited = {k: False for k in graph}
     S.append(start)
     while S:
         u = S.pop()
-        if (not visited[u]):
-            visited[u] = True
+        if (u in visited and not visited[u]):
             visited_order.append(u)
-            try:
+            visited[u] = True
+            if u in graph:
                 nodes = graph[u]
-            except:
+            else:
                 continue
-            S.extend((w for w in nodes if not visited[w]))
+            S.extend([w for w in nodes if (w in visited and not visited[w]) or w not in visited_order])
 
-    #if (mode == 'direct'):
-    #    k = 0
-    #    m = 1
-    #elif (mode == 'reverse'):
-    #    k = 1
-    #    m = 0
-    #if visited is None:
-    #    visited = set()
-    #visited.add(start)
-    #leaders[s-1] = lnode
-    #for next in graph[start] - visited:
-    #    DFS(graph, next, mode, visited)
-    return visited, visited_order
+    return visited_order
 
 
 def main():
-    fname = "2sat6.txt"
-    n_vars, sat_conditions = read_2sat(fname)
-    graph = make_graph(n_vars, sat_conditions)
+    fname = "2sat1.txt"
+    graph = read_make_graph(fname)
     return graph
 
 
